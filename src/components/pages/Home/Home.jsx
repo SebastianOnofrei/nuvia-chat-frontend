9e320f90db0fb904a8caf38e5a8995ac192531dc
Sebastian Onofrei <sebastian.onofrei.business@gmail.com>
2025-04-17 09:03:10 +0300

    feat: little refactor..

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom"; // Import useNavigate
 import "./Home.css";
-import FriendsList from "../../organisms/FriendsList/FriendsList";
 
 const Home = () => {
   const navigate = useNavigate(); // Initialize useNavigate
